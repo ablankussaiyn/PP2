830de8faa72a7fc2569bb1f44745110ecccaae02
Ablan Kussaiyn <ablankussain@gmail.com>
2018-02-07 18:12:29 +0600

    second commit

--- Student/Student/Program.cs
+++ Student/Student/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Student
 {
+    class Student
+    {
+        string name;
+        string surname;
+
+        public void Readinfo()
+        {
+            name=Console.ReadLine();
+            surname=Console.ReadLine();
+        }
+
+      
+        public void Printinfo()
+        {
+            Console.WriteLine(name + " " + surname);
+
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Student line = new Student();
+            line.Readinfo();
+            line.Printinfo();
+            Console.ReadKey;
         }
     }
 }
