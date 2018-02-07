655890b7fdf66c54e919e138653c3c86e5375fb4
Ablan Kussaiyn <ablankussain@gmail.com>
2018-02-07 22:25:12 +0600

    minmax 1

--- Lab 2/MinMax/MinMax/Program.cs
+++ Lab 2/MinMax/MinMax/Program.cs
@@ -11,7 +11,28 @@ namespace MinMax
     {
         static void Main(string[] args)
         {
-            
+            int max = 0;
+            int min = 0;
+            string s = File.ReadAllText(@"C:\Users\Аблан\Documents\PP2\Lab 2\MinMax\minmax.txt");
+            string[] arr = s.Split(' ');
+            foreach (var num in arr) 
+            {
+                Int32.TryParse(num, out int a);
+
+                    if (max<a)
+                    {
+                        max = a;
+                    }
+                    if (min>a)
+                    {
+                        min = a;
+                    }
+                
+            }
+            Console.WriteLine("the maximum value = " + max);
+            Console.WriteLine("the minimum value = " + min);
+            Console.ReadKey();
+
         }
     }
 }
