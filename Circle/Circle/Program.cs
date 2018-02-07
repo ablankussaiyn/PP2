e6c872a4a1932f5b0f1464f2a52df05564b8f21b
Ablan Kussaiyn <ablankussain@gmail.com>
2018-02-07 19:27:17 +0600

    circle 1

--- Circle/Circle/Program.cs
+++ Circle/Circle/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace Circle
 {
+    class Circle
+    {
+        const double pi = Math.PI;
+        double radius;
+        public void Area(double r)
+        {
+            this.radius = r;
+            double A = pi * r * r;
+            Console.WriteLine("the area = " + A);
+        }
+        public void Diameter(double r)
+        {
+            this.radius = r;
+            Console.WriteLine("the diameter = " + 2 * r);
+        }
+        public void Circumference(double r)
+        {
+            this.radius = r;
+            double C = 2 * pi * r;
+            Console.WriteLine("the circumference = " + C);
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Circle s = new Circle();
+            int x = Int32.Parse(Console.ReadLine());
+            s.Area(x);
+            s.Circumference(x);
+            s.Diameter(x);
+            Console.ReadKey();
+
         }
     }
 }
