ed65eed886b978aac7c2e38795b94b4119636cad
Ablan Kussaiyn <ablankussain@gmail.com>
2018-02-09 03:12:45 +0600

    Far

--- Lab 2/Complex/Complex/Program.cs
+++ Lab 2/Complex/Complex/Program.cs
@@ -8,27 +8,76 @@ namespace Complex
 {
     class Complex
     {
-        int a;
-        int b;
-        public void Complx() { }
-        public void Complx(int x, int y)
+        public int a, b;
+
+        public Complex(int x, int y)
         {
-            this.a = x;
-            this.b = y;
+            a = x;
+            b = y;
+
         }
-        public void complexadd ()
+
+
+
+        public Complex Add(Complex complex2)
         {
-                       
+            Complex result = new Complex(this.a * complex2.b + complex2.a * this.b, this.b * complex2.b);
+
+            return result;
+        }
+        public Complex Minus(Complex complex2)
+        {
+            Complex result = new Complex(this.a * complex2.b - complex2.a * this.b, this.b * complex2.b);
+
+            return result;
+        }
+        public Complex Multiple(Complex complex2)
+        {
+            Complex result = new Complex(this.a * complex2.a, this.b * complex2.b);
+
+            return result;
+        }
+        public Complex Division(Complex complex2)
+        {
+            Complex result = new Complex(this.a * complex2.b, this.b * complex2.a);
+
+            return result;
+        }
+
+
+        public void Simplify()
+        {
+            int _a = this.a;
+            int _b = this.b;
+            while (_a > 0 && _b > 0)
+            {
+                if (_a > _b)
+                    _a = _a % _b;
+                else
+                    _b = _b % _a;
+            }
+            int nod = _a + _b;
+            a /= nod;
+            b /= nod;
         }
         
     }
-    
     class Program
     {        
         static void Main(string[] args)
         {
-           
-         
+            string s = Console.ReadLine();
+            string[] arr = s.Split();
+            Complex result = new Complex(0, 0);
+            foreach (string t in arr)
+            {
+                string[] a = t.Split('/');
+                Complex cmp = new Complex(int.Parse(arr[0]), int.Parse(arr[1]));
+                result =result+ cmp;
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+
         }
     }
 }
--- Lab 3/FarManager/FarManager/Program.cs
+++ Lab 3/FarManager/FarManager/Program.cs
@@ -3,13 +3,125 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace FarManager
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        static int CONSOLE_SIZE = 28;
+        static void ShowDirectoryInfo(DirectoryInfo directory, int cursor, int first)
         {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            FileSystemInfo[] fss = directory.GetFileSystemInfos();
+            Console.CursorVisible = false;
+            int index = 0;
+            foreach (FileSystemInfo f in fss)
+            {
+                if (index == cursor)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                }
+                else if (f.GetType() == typeof(DirectoryInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.BackgroundColor = ConsoleColor.Red;
+                }
+                if (index >= first && index <= first + CONSOLE_SIZE)
+                    Console.WriteLine(f.Name);
+                index++;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\Аблан\Documents\PP2");
+            int first = 0;
+            int cursor = 0;
+            ShowDirectoryInfo(directory, cursor, first);
+            int n = directory.GetFileSystemInfos().Length;
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    cursor--;
+                    if (cursor < 0)
+                    {
+                        cursor = n - 1;
+                        first = cursor - CONSOLE_SIZE;
+                    }
+                    if (cursor < first)
+                    {
+                        first--;
+                    }
+                }
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    cursor++;
+                    if (cursor == n)
+                    {
+                        cursor = 0;
+                        first = 0;
+                    }
+                    if (cursor > first + CONSOLE_SIZE)
+                    {
+                        first++;
+                    }
+                }
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    if (directory.GetFileSystemInfos()[cursor].GetType() == typeof(DirectoryInfo))
+                    {
+                        directory = (DirectoryInfo)directory.GetFileSystemInfos()[cursor];
+                        cursor = 0;
+                        n = directory.GetFileSystemInfos().Length;
+                        first = 0;
+                    }
+                    else
+                    {
+                        StreamReader sr = new StreamReader(directory.GetFileSystemInfos()[cursor].FullName);
+                        string s = sr.ReadToEnd();
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Clear();
+                        Console.WriteLine(s);
+                        Console.ReadKey();
+                    }
+
+                }
+                if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    if (directory.Parent != null)
+                    {
+                        directory = directory.Parent;
+                        cursor = 0;
+                        n = directory.GetFileSystemInfos().Length;
+                        first = 0;
+                    }
+                    else
+                        break;
+                }
+                if (keyInfo.Key == ConsoleKey.D)
+                {
+                    if (directory.GetFileSystemInfos()[cursor].GetType() == typeof(FileInfo))
+                    {
+                        DirectoryInfo fordelete = new DirectoryInfo(directory.GetFileSystemInfos()[cursor].FullName);
+                        System.IO.File.Delete(fordelete.FullName);
+                    }
+
+                }
+
+
+                ShowDirectoryInfo(directory, cursor, first);
+            }
+
+
         }
     }
 }
